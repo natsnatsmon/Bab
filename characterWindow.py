6eb3165dfa26a5eb2788877eb8a7d1b201057dcc
natsnatsmon <ha6q6v@gmail.com>
2018-06-13 20:24:38 +0900

    mainWindow - 캐릭터창 즐겨찾기 빼고 거의 함

--- characterWindow.py
+++ characterWindow.py
@@ -1,28 +1,33 @@
-from mainWindow import *
+from tkinter import *
+from tkinter import font
+import tkinter.messagebox
 
-def init_CharacterWindowFrame():
-    global characterWindow, frameCharacterInfo, frameCharacterStatus, frameCharacterEquipment
+def init_Frame():
+    global characterWindow, frameInfo, frameStatus, frameEquipment
 
     tmpFont = font.Font(characterWindow, size=12, weight='bold', family='Consolas')
 
-    frameCharacterInfo = LabelFrame(characterWindow, width =400 - 40, height =250 - 40)
-    frameCharacterStatus = LabelFrame(characterWindow, text ="능력치", width =400 - 40, height =250 - 40, font = tmpFont)
-    frameCharacterEquipment = LabelFrame(characterWindow, text ="장착 장비", width =400 - 40, height =250 - 40, font = tmpFont)
+    frameInfo = LabelFrame(characterWindow, width =400 - 40, height =250 - 40)
+    frameStatus = LabelFrame(characterWindow, text ="능력치", width =400 - 40, height =250 - 40, font = tmpFont)
+    frameEquipment = LabelFrame(characterWindow, text ="장착 장비", width =400 - 40, height =250 - 40, font = tmpFont)
 
-    frameCharacterInfo.place(x = 20, y = 20)
-    frameCharacterStatus.place(x=20, y=100)
-    frameCharacterEquipment.place(x=20, y=100)
+    frameInfo.place(x = 20, y = 20)
+    frameStatus.place(x=20, y=100)
+    frameEquipment.place(x=20, y=100)
 
-    frameCharacterInfo.pack_propagate(0)
-    frameCharacterStatus.pack_propagate(0)
-    frameCharacterEquipment.pack_propagate(0)
+    frameInfo.pack_propagate(0)
+    frameStatus.pack_propagate(0)
+    frameEquipment.pack_propagate(0)
 
-    init_CharacterWindowFrame()
+    #init_InfoFrame()
+    #init_StatusFrame()
+    #init_Equipment()
 
 def init_CharacterWindow():
     global characterWindow
-    characterWindow = Tk()
-    characterWindow.geometry("400x600")
+
+    characterWindow = Toplevel()
+    characterWindow.geometry("600x400")
     characterWindow.title("DnF in")
 
     tmpFont = font.Font(characterWindow, size=20, weight ='bold', family ='Consolas')
@@ -33,10 +38,12 @@ def init_CharacterWindow():
     init_Frame()
 
 def init_CharacterUi():
-    connectOpenAPIServer()
+    #connectOpenAPIServer()
     init_CharacterWindow()
 
 def run_CharacterWindow():
+    init_CharacterUi()
+
     global characterWindow
 #    raise_frame(frameCharacter)
     characterWindow.mainloop()
